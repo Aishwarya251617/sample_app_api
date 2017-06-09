f9cb52b9699bcfcb4e3c5c378535d87d9dff2629
untrodden-shivani <shivani.gupta@untroddenlabs.com>
2017-06-09 11:25:30 +0530

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  include SessionsHelper
+  # render html: "hello, world!"
   def hello
-   # render html: "hello, world!"
    render "static_pages/home"
   end
-
-  
-
 end
